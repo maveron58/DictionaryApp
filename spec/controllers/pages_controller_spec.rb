e2bfe5dfd4f8930cce7d0bf5c20f0b3e5409e7aa
Piotr Heiman <piotr.heilman@gmail.com>
2013-05-28 15:05:00 +0200

    added some tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,27 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
-  describe "GET 'index'" do
+  describe "homepage" do
     it "returns http success" do
       get 'index'
       response.should be_success
+    end
+
+    it "should have the right title (in pl)" do
+      get 'index'
+      assert_select "title", "Słownik Online - Piotr Heilman"
+    end
+
+    it "should have the right title (in pl)" do
+      get 'index', :locale => 'pl'
+      assert_select "title", "Słownik Online - Piotr Heilman"
+    end
+
+    it "should have the right title (in en)" do
+      get 'index', :locale => 'en'
+      assert_select "title", "Online Dictionary - Piotr Heilman"
     end
   end
 
