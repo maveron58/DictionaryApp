92878a0ea47dcafab88d4b18274d04fb1eba4f52
Piotr Heiman <piotr.heilman@gmail.com>
2013-05-31 20:13:20 +0200

    added more dictionary model tests

--- spec/models/dictionary_spec.rb
+++ spec/models/dictionary_spec.rb
@@ -1,42 +1,42 @@
 require 'spec_helper'
+include ActionDispatch::TestProcess
 
 describe Dictionary do
-  before { @dict = Dictionary.new(:file => "sljfldsjflkjdsflkjsdfl") }
+  before { @dict = Dictionary.new(:file => fixture_file_upload('cos.yml')) }
 
   subject { @dict }
 
   it { should respond_to :file }
-  it { should respond_to :name }
 
   it { should be_valid }
 
   describe "when file is not present" do
-    before { @dict.file = "" }
+    before { @dict.file = nil }
     it { should_not be_valid }
   end
 
   describe "when file type is not allowed" do
-    before { @dict.file = "aaa.img" }
+    before { @dict.file = fixture_file_upload('img1.png') }
     it { should_not be_valid }
   end
 
   describe "when file type is not allowed" do
-    before { @dict.file = "aaa.gif" }
+    before { @dict.file = fixture_file_upload('img2.png') }
     it { should_not be_valid }
   end
 
   describe "when file type is not allowed" do
-    before { @dict.file = "aaa.csvcsv" }
+    before { @dict.file = fixture_file_upload('test.zip') }
     it { should_not be_valid }
   end
 
   describe "when file type is allowed" do
-    before { @dict.file = "aaa.csv" }
+    before { @dict.file = fixture_file_upload('cos2.csv') }
     it { should be_valid }
   end
 
   describe "when file type is allowed" do
-    before { @dict.file = "aaa.yml" }
+    before { @dict.file = fixture_file_upload('cos.yml') }
     it { should be_valid }
   end
 end
